cd9e70ec61cecc359d323e5f9d3bc64ef64d8280
Твоє numbmn2n <pechkursasha@gmail.com>
2025-03-23 14:25:47 +0200

    feat: add style to navbar

--- store_frontend/src/components/Navbar/Navbar.tsx
+++ store_frontend/src/components/Navbar/Navbar.tsx
@@ -1,22 +1,49 @@
 import { Link } from "react-router-dom";
+import FF_logo from "../../assets/FF_logo.svg";
 export default function Navbar() {
   return (
     <>
-      <nav className="bg-green-300 border-gray-200" >
-        <ul className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="/about">About</Link>
-          <Link to="/products">
-            Products
-          </Link>
-          <Link to="/product">
-            Product_Info
-          </Link>
-          <Link to="/cards">
-            Cards
-          </Link>
-          <Link to="/profile">
-            Profile
-          </Link>
+      <nav className="bg-lime-700 flex  items-center px-6 py-4 justify-between">
+        <div className="w-25 h-20">
+          <img
+            src={FF_logo}
+            alt="Logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
+        <ul className="space-x-40 pr-10 flex items-center ">
+          <li>
+            <Link
+              className="px-6 py-3 text-white rounded-sm hover:bg-yellow-300/20 transition"
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="px-6 py-3  text-white rounded-sm hover:bg-yellow-300/20 transition "
+              to="/product"
+            >
+              Product_Info
+            </Link>
+          </li>
+          <li>
+            <Link
+              className=" px-6 py-3 text-white rounded-sm  transition hover:bg-yellow-300/20 hower: transition "
+              to="/cards"
+            >
+              Cards
+            </Link>
+          </li>
+          <li>
+            <Link
+              className=" px-6 py-3 block text-white  rounded-sm hover:bg-yellow-300/20 transition "
+              to="/profile"
+            >
+              Profile
+            </Link>
+          </li>
         </ul>
       </nav>
     </>
